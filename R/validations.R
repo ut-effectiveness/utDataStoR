d07bc8dc6c30d529f49b4c3c8e4f11877477d484
jazzyjpfeifer@gmail.com <jazzyjpfeifer@gmail.com>
2023-01-12 09:09:32 -0700

    Added validation sql files

--- R/validations.R
+++ R/validations.R
@@ -16,3 +16,93 @@ sql_validate_student <- function(data_source, name) {
   fs::file_copy(here::here('inst', 'sql', file),
                 here::here('sql', name))
 }
+
+#' `sql_validate_student_courses` will generate the standard sql query for pulling Student Course Validations from Edify.
+#' Validations are done through Edify.
+#'
+#' @param data_source The server you will be full from. Edify
+#' @param name The name you want the SQL file to have in your sql folder.
+#'
+#' @return A sql file in your SQL folder
+#' @export
+#'
+
+sql_validate_student_courses <- function(data_source, name) {
+  file <- 'sql_validate_student_courses.sql'
+
+  fs::file_copy(here::here('inst', 'sql', file),
+                here::here('sql', name))
+}
+
+#' `sql_validate_courses` will generate the standard sql query for pulling Course Validations from Edify.
+#' Validations are done through Edify.
+#'
+#' @param data_source The server you will be full from. Edify
+#' @param name The name you want the SQL file to have in your sql folder.
+#'
+#' @return A sql file in your SQL folder
+#' @export
+#'
+
+sql_validate_courses <- function(data_source, name) {
+  file <- 'sql_validate_courses.sql'
+
+  fs::file_copy(here::here('inst', 'sql', file),
+                here::here('sql', name))
+}
+
+#' `sql_validate_graduation` will generate the standard sql query for pulling Graduation Validations from Edify.
+#' Validations are done through Edify.
+#'
+#' @param data_source The server you will be full from. Edify
+#' @param name The name you want the SQL file to have in your sql folder.
+#'
+#' @return A sql file in your SQL folder
+#' @export
+#'
+
+sql_validate_graduation <- function(data_source, name) {
+  file <- 'sql_validate_graduation.sql'
+
+  fs::file_copy(here::here('inst', 'sql', file),
+                here::here('sql', name))
+}
+
+#' `sql_validate_rooms` will generate the standard sql query for pulling Rooms Validations from Edify.
+#' Validations are done through Edify.
+#'
+#' @param data_source The server you will be full from. Edify
+#' @param name The name you want the SQL file to have in your sql folder.
+#'
+#' @return A sql file in your SQL folder
+#' @export
+#'
+
+sql_validate_rooms <- function(data_source, name) {
+  file <- 'sql_validate_rooms.sql'
+
+  fs::file_copy(here::here('inst', 'sql', file),
+                here::here('sql', name))
+}
+
+#' `sql_validate_buildings` will generate the standard sql query for pulling Building Validations from Edify.
+#' Validations are done through Edify.
+#'
+#' @param data_source The server you will be full from. Edify
+#' @param name The name you want the SQL file to have in your sql folder.
+#'
+#' @return A sql file in your SQL folder
+#' @export
+#'
+
+sql_validate_buildings <- function(data_source, name) {
+  file <- 'sql_validate_buildings.sql'
+
+  fs::file_copy(here::here('inst', 'sql', file),
+                here::here('sql', name))
+}
+
+
+
+
+
