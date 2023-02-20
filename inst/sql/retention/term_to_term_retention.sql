eca42957070370877b8713c05f0cb137433a7189
Matt Nickodemus <matthewnickodemus@gmail.com>
2023-02-19 19:51:06 -0700

    added term to term retention script

---
+++ inst/sql/retention/term_to_term_retention.sql
@@ -0,0 +1,43 @@
+/*
+ Term to term retention
+ */
+   SELECT a.student_id,
+          a.term_id,
+          a.is_returned_next_spring,
+          a.is_returned_next_fall,
+          c.primary_major_desc,
+          c.primary_degree_id,
+          c.primary_degree_desc,
+          d.college_abbrv,
+          d.college_desc,
+          e.first_name,
+          e.last_name,
+          e.gender_code,
+          e.ipeds_race_ethnicity,
+          e.is_veteran,
+          e.is_international,
+          e.is_athlete,
+          e.is_first_generation,
+          COALESCE(f.is_exclusion, FALSE) AS is_exclusion,
+          COALESCE(f.cohort_start_term_id, 'None') AS cohort_start_term_id,
+          COALESCE(f.cohort_desc, 'None') AS cohort_type
+     FROM export.student_term_outcome a
+LEFT JOIN export.term b
+       ON b.term_id = a.term_id
+LEFT JOIN export.student_term_level_version c
+       ON c.student_id = a.student_id
+      AND c.term_id = a.term_id
+      AND c.is_enrolled
+      AND c.is_primary_level
+      AND c.is_census_version
+LEFT JOIN export.academic_programs d
+       ON d.program_id = c.primary_program_id
+LEFT JOIN export.student e
+       ON e.student_id = a.student_id
+LEFT JOIN export.student_term_cohort f
+       ON f.student_id = a.student_id
+      AND f.cohort_desc IN ('First-Time Freshman', 'Transfer')
+    WHERE b.season = 'Fall'
+      AND substr(a.term_id, 1, 4)::int >= (SELECT substr(term_id, 1, 4)::int - 5
+                                     FROM export.term
+                                     WHERE is_current_term)
