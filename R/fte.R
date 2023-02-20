045bd1d24accbc93e7b837717f67da1fa5f90f21
Matt Nickodemus <matthewnickodemus@gmail.com>
2023-02-19 21:47:56 -0700

    Checking to see if documentation for function changes

--- R/fte.R
+++ R/fte.R
@@ -3,7 +3,7 @@
 #' `sql_fte` will generate the standard sql query for pulling FTE from Edify, REPT
 #' of DSCIR.
 #'
-#' @param data_source The server you will be full from. Edify, REPT, DSCIR
+#' @param data_source Bob. The server you will be full from. Edify, REPT, DSCIR
 #' @param name The name you want the SQL file to have in your sql folder.
 #'
 #' @return A sql file in your SQL folder
