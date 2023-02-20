62096779f41c1481149f44766ad549628ef33ef7
Matt Nickodemus <matthewnickodemus@gmail.com>
2023-02-19 22:38:37 -0700

    This should fix the issue. The problem was pulling from the inst file.

--- R/fte.R
+++ R/fte.R
@@ -3,16 +3,27 @@
 #' `sql_fte` will generate the standard sql query for pulling FTE from Edify, REPT
 #' of DSCIR.
 #'
-#' @param data_source Bob. The server you will be full from. Edify, REPT, DSCIR
+#' @param data_source The server you will be full from. Edify, REPT, DSCIR
 #' @param name The name you want the SQL file to have in your sql folder.
 #'
 #' @return A sql file in your SQL folder
 #' @export
 #'
 
-sql_fte <- function(data_source, name) {
-  file <- 'fte_off_edify.sql'
+sql_fte <- function(data_source = 'Edify', name) {
 
-  fs::file_copy(here::here('sql', file),
-            here::here('sql', name))
+  base <- system.file('sql', package='utDataStoR')
+
+  file <- paste(
+    base,
+    '/',
+    'retention',
+    '/',
+    'fte_off_edify.sql',
+    sep = ''
+  )
+
+  fs::file_copy(
+    file,
+    here::here('sql', name))
 }
