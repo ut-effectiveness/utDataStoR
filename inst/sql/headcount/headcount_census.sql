bccb34fa7b03ce752a2c1ac655115dc1db8552eb
JoyBaule <52751421+JoyBaule@users.noreply.github.com>
2023-06-14 16:27:29 -0600

    streamlined SQL to match metrics

--- inst/sql/headcount/headcount_census.sql
+++ inst/sql/headcount/headcount_census.sql
@@ -2,12 +2,15 @@
 Census/3rd Week headcount from Edify
 */
 
-   SELECT a.term_desc,
-           COUNT(a.sis_system_id)
-      FROM export.student_term_level_version a
-     WHERE a.is_enrolled IS TRUE
-       AND a.term_id >= '201940'
-       AND a.is_primary_level IS TRUE
-       AND a.version_desc = 'Census'
-  GROUP BY a.term_desc, a.term_id, a.version_desc
-  ORDER BY a.term_id desc;
+SELECT a.term_desc,
+          COUNT(a.sis_system_id) AS census_headcount
+     FROM export.student_term_level_version a
+LEFT JOIN export.term b
+       ON a.term_id = b.term_id
+    WHERE a.is_enrolled IS TRUE
+      AND a.is_primary_level IS TRUE
+      AND a.version_desc = 'Census'
+      AND DATE_PART('year', NOW()) - b.academic_year_code :: INT <= 5 -- Last 5 Years
+      AND b.season IN ('Fall', 'Spring') -- Fall and Spring only
+ GROUP BY a.term_desc
+ ORDER BY a.term_desc;
