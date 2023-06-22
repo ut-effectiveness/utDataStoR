5ef6c6dcca972edf2c4462d82343e54f740b3fed
JoyBaule <52751421+JoyBaule@users.noreply.github.com>
2023-06-22 12:56:11 -0600

    Updating narrative on vignette.

--- inst/sql/headcount/headcount_census.sql
+++ inst/sql/headcount/headcount_census.sql
@@ -2,15 +2,14 @@
 Census/3rd Week headcount from Edify
 */
 
-SELECT a.term_desc,
-          COUNT(a.sis_system_id) AS census_headcount
+   SELECT a.term_desc,
+          COUNT(a.sis_system_id) AS current_headcount
      FROM export.student_term_level_version a
 LEFT JOIN export.term b
        ON a.term_id = b.term_id
     WHERE a.is_enrolled IS TRUE
       AND a.is_primary_level IS TRUE
-      AND a.version_desc = 'Census'
-      AND DATE_PART('year', NOW()) - b.academic_year_code :: INT <= 5 -- Last 5 Years
-      AND b.season IN ('Fall', 'Spring') -- Fall and Spring only
+      AND a.version_desc = 'Current'
+      AND DATE_PART('year', NOW()) - b.academic_year_code :: INT <= 5 -- Current term plus last 5 Years.
  GROUP BY a.term_desc
  ORDER BY a.term_desc;
--- inst/sql/headcount/headcount_current.sql
+++ inst/sql/headcount/headcount_current.sql
@@ -2,7 +2,7 @@
 Current headcount from Edify.
 */
 
-SELECT a.term_desc,
+   SELECT a.term_desc,
           COUNT(a.sis_system_id) AS current_headcount
      FROM export.student_term_level_version a
 LEFT JOIN export.term b
@@ -11,6 +11,5 @@ LEFT JOIN export.term b
       AND a.is_primary_level IS TRUE
       AND a.version_desc = 'Current'
       AND DATE_PART('year', NOW()) - b.academic_year_code :: INT <= 5 -- Last 5 Years
-      AND b.season IN ('Fall', 'Spring') -- Fall and Spring only
  GROUP BY a.term_desc
  ORDER BY a.term_desc;
