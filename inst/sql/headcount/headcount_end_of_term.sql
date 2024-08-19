5eaeaf6f550857245eb914b6205b93fb1898ed84
Andrea <63427856+andreabr-dixie@users.noreply.github.com>
2024-08-19 12:50:02 -0600

    headcount_end_of_term changes - changed the description (removed "from Edify"; changed a.sis_system_id to a.student_id for consistency; removed line 14 that included only Fall and Spring and excluded summer for consistency.

--- inst/sql/headcount/headcount_end_of_term.sql
+++ inst/sql/headcount/headcount_end_of_term.sql
@@ -1,16 +1,15 @@
 /*
-End of Term headcount from Edify
+End of Term headcount
 */
 
 SELECT a.term_desc,
-          COUNT(a.sis_system_id) AS current_headcount
-     FROM export.student_term_level_version a
-LEFT JOIN export.term b
-       ON a.term_id = b.term_id
-    WHERE a.is_enrolled IS TRUE
-      AND a.is_primary_level IS TRUE
-      AND a.version_desc = 'End of Term'
-      AND DATE_PART('year', NOW()) - b.academic_year_code :: INT <= 5 -- Last 5 Years
-      AND b.season IN ('Fall', 'Spring') -- Fall and Spring only
- GROUP BY a.term_desc
- ORDER BY a.term_desc;
+       COUNT(a.student_id) AS current_headcount
+FROM export.student_term_level_version a
+         LEFT JOIN export.term b
+                   ON a.term_id = b.term_id
+WHERE a.is_enrolled IS TRUE
+  AND a.is_primary_level IS TRUE
+  AND a.version_desc = 'End of Term'
+  AND DATE_PART('year', NOW()) - b.academic_year_code :: INT <= 5 -- Current year plus last 5 years
+GROUP BY a.term_desc
+ORDER BY a.term_desc;
