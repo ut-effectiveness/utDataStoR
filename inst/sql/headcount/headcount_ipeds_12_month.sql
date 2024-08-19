099f6e27fa4007b62b2dac0311607ec72ad7e544
Andrea <63427856+andreabr-dixie@users.noreply.github.com>
2024-08-19 12:56:37 -0600

    headcount_ipeds_12_month changes: changed a.sis_system_id to a.student_id for consistency; removed the hard coding of academic year and changed to current academic year minus 1 making it for the previous year as IPEDS requires; added additional documentation.

--- inst/sql/headcount/headcount_ipeds_12_month.sql
+++ inst/sql/headcount/headcount_ipeds_12_month.sql
@@ -2,14 +2,14 @@
 
  */
 
-  SELECT b.academic_year_code,
-         COUNT(DISTINCT a.sis_system_id)
-     FROM export.student_term_level_version a
-LEFT JOIN export.term b
-       ON a.term_id = b.term_id
-    WHERE a.is_enrolled = TRUE
-      AND a.is_primary_level = TRUE
-      AND a.version_desc = 'Census'
-      AND b.academic_year_code = '2022'
-      AND b.season != 'Summer'
- GROUP BY academic_year_code;
+SELECT b.academic_year_code,
+       COUNT(DISTINCT a.student_id) --unduplicated students over the course of an academic year
+FROM export.student_term_level_version a
+         LEFT JOIN export.term b
+                   ON a.term_id = b.term_id
+WHERE a.is_enrolled = TRUE
+  AND a.is_primary_level = TRUE
+  AND a.version_desc = 'Census'
+  AND DATE_PART('year', NOW()) - b.academic_year_code :: INT = 1 -- Last academic year
+  AND b.season != 'Summer'                                       -- remove summer as per ipeds requirement
+GROUP BY academic_year_code;
