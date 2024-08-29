51c4dc787c093f8dd127d0678f970eda76ed8ad0
Andrea <63427856+andreabr-dixie@users.noreply.github.com>
2024-08-29 11:49:20 -0600

    added back fte_off_dscir.sql

---
+++ inst/sql/fte/fte_off_dscir.sql
@@ -0,0 +1,37 @@
+--DSCIR FTE
+
+SELECT ( -- FTE - Undergraduate Level
+                  SELECT round(sum(sc_att_cr)/15 + sum(nvl(sc_contact_hrs,0))/45,2)
+                  FROM   student_courses sc, courses c
+                  WHERE  sc.dsc_crn   = c.dc_crn
+                  AND    sc.dsc_term_code = c.dsc_term_code
+                  AND    sc.dsc_term_code = '202143'
+                  AND    c_level != 'G'
+              )+(
+                  -- FTE - Graduate Level
+                  SELECT nvl(round(sum(sc_att_cr)/10 + sum(nvl(sc_contact_hrs,0))/45,2),0)
+                  FROM   student_courses sc, courses c
+                  WHERE  sc.dsc_crn   = c.dc_crn
+                  AND    sc.dsc_term_code = c.dsc_term_code
+                  AND    sc.dsc_term_code = '202143'
+                  AND    c_level = 'G'
+                ) AS FTE
+        FROM    DUAL;
+
+SELECT ( -- FTE - Undergraduate Level
+                  SELECT round(sum(sc_att_cr)/15)
+                  FROM   student_courses sc, courses c
+                  WHERE  sc.dsc_crn   = c.dc_crn
+                  AND    sc.dsc_term_code = c.dsc_term_code
+                  AND    sc.dsc_term_code = '202143'
+                  AND    c_level != 'G'
+              )+(
+                  -- FTE - Graduate Level
+                  SELECT round(sum(sc_att_cr)/10)
+                  FROM   student_courses sc, courses c
+                  WHERE  sc.dsc_crn   = c.dc_crn
+                  AND    sc.dsc_term_code = c.dsc_term_code
+                  AND    sc.dsc_term_code = '202143'
+                  AND    c_level = 'G'
+                ) AS FTE
+        FROM    DUAL;
