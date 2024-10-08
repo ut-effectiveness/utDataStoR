f199741291d4dd73e8a21f4458654b95a1f88915
Andrea <63427856+andreabr-dixie@users.noreply.github.com>
2024-10-08 12:36:47 -0600

    Made changes to the term_to_term_retention.sql Added the is_credential_completer field needed to calcuate return rate. Made changes for consitency with other sql's in utDataStoR. Added fields from student term level that aid in looking at retention rate (like full and part-time status).

--- inst/sql/retention/term_to_term_retention.sql
+++ inst/sql/retention/term_to_term_retention.sql
@@ -1,5 +1,7 @@
 /*
  Term to term retention
+ Uses student term outcome as the base table. Includes students enrolled as of census.
+ Includes data to calculate Term to Term Retention (both Come Back rate and Return rate).
  */
    SELECT a.student_id,
           a.term_id,
@@ -8,12 +10,21 @@
           SUBSTRING(a.term_id, 1, 4) AS year,
           a.is_returned_next_spring,
           a.is_returned_next_fall,
-          a.is_degree_seeking,
+          a.is_graduated
+            OR a.is_degree_completer_certificate
+            OR a.is_degree_completer_associates
+            OR a.is_degree_completer_bachelors
+            OR a.is_degree_completer_masters
+            OR a.is_degree_completer_doctorate AS is_credential_completer,
           c.primary_major_desc,
           c.primary_degree_id,
           c.primary_degree_desc,
           d.college_abbrv,
           d.college_desc,
+          a.is_degree_seeking,
+          c.level_desc,
+          c.student_type_desc,
+          c.full_time_part_time_code,
           e.gender_code,
           CASE
            WHEN e.gender_code = 'M' THEN 'Male'
@@ -27,16 +38,16 @@
           e.is_first_generation,
           COALESCE(f.is_exclusion, FALSE) AS is_exclusion,
           COALESCE(f.cohort_start_term_id, 'None') AS cohort_start_term_id,
-          COALESCE(f.cohort_desc, 'None') AS cohort_type
+          COALESCE(f.cohort_desc, 'None') AS cohort_desc
      FROM export.student_term_outcome a
 LEFT JOIN export.term b
        ON b.term_id = a.term_id
 LEFT JOIN export.student_term_level_version c
        ON c.student_id = a.student_id
       AND c.term_id = a.term_id
-      AND c.is_enrolled
-      AND c.is_primary_level
-      AND c.is_census_version
+      AND c.is_enrolled IS TRUE
+      AND c.is_primary_level IS TRUE
+      AND c.is_census_version IS TRUE
 LEFT JOIN export.academic_programs d
        ON d.program_id = c.primary_program_id
 LEFT JOIN export.student e
@@ -44,8 +55,6 @@ LEFT JOIN export.student e
 LEFT JOIN export.student_term_cohort f
        ON f.student_id = a.student_id
       AND f.cohort_desc IN ('First-Time Freshman', 'Transfer')
-    WHERE a.is_enrolled_census
+    WHERE a.is_enrolled_census IS TRUE
       AND b.season = 'Fall'
-      AND substr(a.term_id, 1, 4)::int >= (SELECT substr(term_id, 1, 4)::int - 5
-                                     FROM export.term
-                                     WHERE is_current_term)
+      AND DATE_PART('year', NOW()) - b.academic_year_code :: INT <= 5 -- Current year plus last 5 years
