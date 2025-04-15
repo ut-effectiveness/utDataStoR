4fa2672b9f881f8dcf73c2388b3f4e49ae431f22
Andrea <63427856+andreabr-dixie@users.noreply.github.com>
2025-04-15 16:33:38 -0600

    Reviewed graduation_dashboard.sql and noticed I could remove the join to student_term_level (b table) and made the adjustment.

--- inst/sql/graduation/graduation_dashboard.sql
+++ inst/sql/graduation/graduation_dashboard.sql
@@ -1,4 +1,4 @@
-Tableau Credentials Awarded Dashboard:
+--Tableau Credentials Awarded Dashboard:
 
 /* Graduation SQL from export for the Tableau credentials awarded dashboard */
 
@@ -16,10 +16,10 @@ c.is_veteran,
 c.gender_code,
 c.ipeds_race_ethnicity
 FROM export.degrees_awarded a
-LEFT JOIN export.student_term_level b
-ON b.student_id = a.student_id
-AND b.level_id = a.level_id
-AND b.term_id = a.graduated_term_id
+--LEFT JOIN export.student_term_level b
+--ON b.student_id = a.student_id
+--AND b.level_id = a.level_id
+--AND b.term_id = a.graduated_term_id
 LEFT JOIN export.student c
 ON c.student_id = a.student_id
 LEFT JOIN export.academic_programs d
@@ -28,3 +28,4 @@ LEFT JOIN export.term e
 ON e.term_id = a.graduated_term_id
 WHERE a.degree_status_code = 'AW'
 AND a.graduated_term_id >= '201230'
+
